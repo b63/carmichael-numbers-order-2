1a4480722ce5ac8c037e28ea53ced4d5d8299719
b63 <avink5760@gmail.com>
2020-05-29 09:08:19 -0500

    added Makefile

--- generate_cprimes.cpp
+++ generate_cprimes.cpp
@@ -66,7 +66,6 @@ int main() {
 
 	// subset products of filetered_primes are possible carmicheal numbers
 	// go over every subset of size factors, store results in cprime list
-	int filtered_size = filtered_primes.size();
 	std::vector< std::vector<int> > cprimes;
 	subset_product(filtered_primes, cprimes);
 
@@ -187,11 +186,11 @@ void filter_primes(const std::vector<int> &primes, std::vector<int> &dest)
 // is stored in the vector cprimes.
 void subset_product(const std::vector<int> &primes, std::vector< std::vector<int> > &cprimes)
 {
-	int num_primes = primes.size();
+	size_t num_primes = primes.size();
 	// go through all possible subset sizes starting from 2
-	for (int t=2; t <= num_primes; ++t)
+	for (size_t t=2; t <= num_primes; ++t)
 	{
-		int factors = t;
+		unsigned int factors = t;
 		std::vector<int> index_stack = {0};
 		std::cout << "checking subsets of size " << t << " ...";
 		std::cout <<  "(found " << cprimes.size() << " till now)\n";
@@ -199,14 +198,14 @@ void subset_product(const std::vector<int> &primes, std::vector< std::vector<int
 		// go through subsets of size factors suing a stack
 		while (index_stack.size() > 0)
 		{
-			int top_i = index_stack.size() - 1;
+			size_t top_i = index_stack.size() - 1;
 			// initializaiton
 			if (index_stack[top_i] == -1)
 			{
 				index_stack[top_i] = index_stack.size() > 1 ? index_stack[top_i-1]+1 : 0;
 			}
 
-			if (index_stack[top_i] < num_primes)
+			if ((size_t) index_stack[top_i] < num_primes)
 			{
 				if (index_stack.size() < factors)
 				{
@@ -217,14 +216,14 @@ void subset_product(const std::vector<int> &primes, std::vector< std::vector<int
 				// check that this subset is a carmicheal prime
 				bool carmicheal = true;
 				NTL::ZZ prod(1);
-				for (int j = 0; j < factors; ++j)
+				for (size_t j = 0; j < factors; ++j)
 				{
 					prod *= primes[index_stack[j]];
 				}
 
 				// check that every prime factor satisfies the following divisibility property
 				//     p^r -1 divides n, where r ranges from 1,..,ORDER
-				for (int j = 0; j < factors; ++j)
+				for (size_t j = 0; j < factors; ++j)
 				{
 					int p_factor = primes[index_stack[j]];
 					NTL::ZZ p_r(1);

0a86538e944e503f3d0baacc6784ce370fc8733e
b63 <avink5760@gmail.com>
2020-07-23 16:29:36 -0500

    finished include_as_factor

--- src/gen_nonrigid.cpp
+++ src/gen_nonrigid.cpp
@@ -8,8 +8,19 @@ int
 main(int argc, char **argv)
 {
     Factorization f1 { {2, 3, 5, 7}, {2, 2, 2, 2} };
-    Factorization f2 { {2, 3, 5, 7}, {2, 2, 2, 2} };
+    Factorization f2 { {2, 11},      {3, 2} };
 
     std::cout << f1 << "\n" << f2 << "\n";
     std::cout << include_as_factor(f1, f2) << "\n";
+}
+
+void
+test_include_as_factor()
+{
+}
+
+bool
+check_includ_as_factor(const Factorization& res, const Factorization& n, const Factorization& factor)
+{
+
 }

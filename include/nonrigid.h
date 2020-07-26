9601d806a09d30e86467e4d806eb13b6f55e484f
b63 <avink5760@gmail.com>
2020-07-25 19:01:18 -0500

    finished subset prod brute force

--- include/nonrigid.h
+++ include/nonrigid.h
@@ -1,4 +1,12 @@
 #ifndef RIGID_H
 #define RIGID_H
 
+#include <vector>
+#include <util.h>
+
+
+void generate_nonrigid_cprimes(long p0, long p1, const Factorization &L_fact,
+        const Factorization &M_fact, long sieve_size);
+void subset_product_brute_force(std::vector<std::vector<size_t>> &cprimes, const std::vector<long> &primes);
+
 #endif

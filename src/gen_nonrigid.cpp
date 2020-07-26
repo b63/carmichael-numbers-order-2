15fa81c3a4a414031cd9203ca534fa00d201fd02
b63 <avink5760@gmail.com>
2020-07-25 19:01:18 -0500

    finished subset prod brute force

--- src/gen_nonrigid.cpp
+++ src/gen_nonrigid.cpp
@@ -7,20 +7,7 @@
 int
 main(int argc, char **argv)
 {
-    Factorization f1 { {2, 3, 5, 7}, {2, 2, 2, 2} };
-    Factorization f2 { {2, 11},      {3, 2} };
-
-    std::cout << f1 << "\n" << f2 << "\n";
-    std::cout << include_as_factor(f1, f2) << "\n";
-}
-
-void
-test_include_as_factor()
-{
-}
-
-bool
-check_includ_as_factor(const Factorization& res, const Factorization& n, const Factorization& factor)
-{
-
+    std::vector<std::vector<size_t> > cprimes;
+    std::vector<long> primes {1, 2, 3, 4};
+    subset_product_brute_force(cprimes, primes);
 }

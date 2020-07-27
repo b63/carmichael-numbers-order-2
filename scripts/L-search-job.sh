7afcc7a4cd0e8f827aac49d3940a1d994c90454d
b63 <avink5760@gmail.com>
2020-07-27 12:40:29 -0500

    added more automation scripts

---
+++ scripts/L-search-job.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#PBS -l walltime=120:00:00
+#PBS -N high_density_search
+#
+# Email address for notifications
+#PBS -M bkoirala@iwu.edu
+#
+# Set up a job array
+#PBS -t 0-10
+#
+# store stderr and stdout in joboutput directory
+#PBS -e jobstreams/err
+#PBS -o jobstreams/out
+
+
+
+
+DIR=/home/bkoirala/repos/summer-2020-research
+OUTPUT_DIR=$DIR/data/outputs
+PROGRESS_DIR=$DIR/data/progress
+INPUT_DATA=$DIR/data/jobdata
+BIN=$DIR/build
+
+PARAMS=('-m1' '-l10000')
+
+[[ -f $OUTPUT_DIR ]] || mkdir -p $OUTPUT_DIR
+[[ -f $PROGRESS_DIR ]] || mkdir -p $PROGRESS_DIR
+
+if [[ ! -f "$INPUT_DATA/job$PBS_ARRAYID" ]]; then
+    echo "no data found: $INPUT_DATA/job$PBS_ARRAYID" >&2
+    exit 1
+fi
+
+i=0
+while read line;
+do
+    IFS=' ' read -r -a vals <<< "$line"
+
+    params=("${PARAMS[@]}" "${vals[@]}")
+    stdbuf -e0 -o0 $BIN/construct_P "${params[@]}" > "$OUTPUT_DIR/out$PBS_ARRAYID-$i" 2>> $PROGRESS_DIR/job$PBS_ARRAYID
+
+    i=$(($i+1))
+done < "$INPUT_DATA/job$PBS_ARRAYID"
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -14,12 +14,14 @@ done
 
 echo -e "Cleaning data/interpolation ..."
 rm -fv data/interpolation/*
+echo -e "Cleaning data/outputs ..."
+rm -fv data/outputs/*
 echo -e "\nCleaning data/progress ..."
 rm -fv data/progress/*
 
 if [[ $jobdata -eq 1 ]]; then
     echo -e "\nCleaning data/jobdata ..."
-    rm -fv data/progress/*
+    rm -fv data/jobdata/*
 fi
 
 echo -e "\nCleaning jobstreams/err ..."

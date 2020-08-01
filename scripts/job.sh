5a44fc433d0735701c03c7def984691d339898c1
b63 <avink5760@gmail.com>
2020-08-01 17:43:58 -0500

    writes progress to progress file

--- scripts/job.sh
+++ scripts/job.sh
@@ -6,7 +6,7 @@
 #PBS -M bkoirala@iwu.edu
 #
 # Set up a job array
-#PBS -t 0-49
+#PBS -t 0-71
 #
 # store stderr and stdout in joboutput directory
 #PBS -e jobstreams/err
@@ -15,39 +15,18 @@
 
 
 DIR=/home/bkoirala/repos/summer-2020-research
-OUTPUT_DIR=$DIR/data/interpolation
+OUTPUT_DIR=$DIR/data/intrp
 INPUT_DATA=$DIR/data/jobdata
 PROGRESS_DIR=$DIR/data/progress
 BIN=$DIR/build
 
-PARAMS=('-l10000000')
 
 [[ -f $OUTPUT_DIR ]] || mkdir -p $OUTPUT_DIR
-[[ -f $PROGRESS_DIR ]] || mkdir -p $PROGRESS_DIR
 
 if [[ ! -f "$INPUT_DATA/job$PBS_ARRAYID" ]]; then
     echo "no data found: $INPUT_DATA/job$PBS_ARRAYID" >&2
     exit 1
 fi
 
-pstring=$(head  -n 1 $INPUT_DATA/all)
-IFS=', ' read -r -a primes <<< "$pstring"
-
-while read line;
-do
-    IFS=':' read -r -a vals <<< "$line"
-    dist_index=${vals[0]}
-    powers=${vals[1]}
-
-    params=("${PARAMS[@]}")
-    IFS=', ' read -r -a array <<< "$powers"
-    for index in "${!array[@]}";
-    do
-        params+=( "${primes[index]}^${array[index]}" )
-    done
-
-    stdbuf -e0 -o0 echo "$dist_index, ${powers[@]}" >> "$PROGRESS_DIR/job$PBS_ARRAYID"
-    $BIN/calc_density "${params[@]}" > "$OUTPUT_DIR/out$dist_index" 2>/dev/null
-
-    i=$(($i+1))
-done < "$INPUT_DATA/job$PBS_ARRAYID"
+PARAMS=("10000000" "$INPUT_DATA/job$PBS_ARRAYID" "$INPUT_DATA/all" "$OUTPUT_DIR/out$PBS_ARRAYID" "$PROGRESS_DIR/job$PBS_ARRAYID")
+$BIN/calc_density_batch "${PARAMS[@]}"

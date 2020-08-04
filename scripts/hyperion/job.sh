244f8eb9c50362ef8bdc3cb78ebfcc7df111b4be
b63 <avink5760@gmail.com>
2020-08-03 21:20:21 -0500

    updated job script to run partial jobs

--- scripts/hyperion/job.sh
+++ scripts/hyperion/job.sh
@@ -6,7 +6,7 @@
 #PBS -M bkoirala@iwu.edu
 #
 # Set up a job array
-#PBS -t 0-71
+#PBS -t 0-2
 #
 # store stderr and stdout in joboutput directory
 #PBS -e jobstreams/err
@@ -23,10 +23,22 @@ BIN=$DIR/build
 
 [[ -f $OUTPUT_DIR ]] || mkdir -p $OUTPUT_DIR
 
-if [[ ! -f "$INPUT_DATA/job$PBS_ARRAYID" ]]; then
-    echo "no data found: $INPUT_DATA/job$PBS_ARRAYID" >&2
-    exit 1
-fi
+i=0
+while read line
+do
+    if [ $i -lt $PBS_ARRAYID ]; then
+        i=$((i+1))
+        continue
+    fi
 
-PARAMS=("10000000" "$INPUT_DATA/job$PBS_ARRAYID" "$INPUT_DATA/all" "$OUTPUT_DIR/out$PBS_ARRAYID" "$PROGRESS_DIR/job$PBS_ARRAYID")
-$BIN/calc_density_batch "${PARAMS[@]}"
+    export PBS_ARRAYID=$line
+    echo "PBS_ARRAYID=$PBS_ARRAYID"
+    if [[ ! -f "$INPUT_DATA/job$PBS_ARRAYID" ]]; then
+        echo "no data found: $INPUT_DATA/job$PBS_ARRAYID" >&2
+        exit 1
+    fi
+
+    PARAMS=("10000000" "$INPUT_DATA/job$PBS_ARRAYID" "$INPUT_DATA/all" "$OUTPUT_DIR/out$PBS_ARRAYID" "$PROGRESS_DIR/job$PBS_ARRAYID")
+    $BIN/calc_density_batch "${PARAMS[@]}"
+    break
+done < $DIR/pbs_ids

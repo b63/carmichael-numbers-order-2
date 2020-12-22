4ddfdef8a9e4a8be6821052ad7839f0ca35ecc8d
b63 <avink5760@gmail.com>
2020-12-21 18:36:45 -0600

    script for checking factors

--- scripts/test.py
+++ scripts/test.py
@@ -6,7 +6,8 @@ f = sys.stdin
 line = f.readline()
 factors = [int(x) for x in line.split(' ')]
 n = reduce(lambda x,y: x*y, factors)
+print('n = {:d}'.format(n))
 for p in factors:
-    print("p = {:d}, n mod p^2-1 = {}".format(p, (n-1)%(p**2-1)))
+    print("p = {:d}, n mod p^2-1 = {}".format(p, (n)%(p**2-1)))
 
 
